30f24ac68f74679f4eeca73b90a64d8ac68bc8d2
Joao Eduardo Luis <joao@1e3ms.io>
2024-02-15 18:41:51 +0000

    foo/store_value: delete data buffer properly
    Signed-off-by: Joao Eduardo Luis <joao@1e3ms.io>

--- src/store_value.hh
+++ src/store_value.hh
@@ -16,17 +16,22 @@ namespace store {
 // The 'store_value' class takes ownership of whatever 'data' pointer it is
 // provided, and will free it up on destruction (via 'delete').
 class store_value {
-  const char* _data;
+  char* _data;
   size_t _size;
 
  public:
-  store_value(const char* data, size_t size) : _data(data), _size(size) {}
+  // copies contents of the provided 'data' pointer to a new memory location, to
+  // be owned by this store_value
+  store_value(const char* data, size_t size) : _size(size) {
+    _data = new char[size];
+    memcpy(_data, data, size);
+  }
 
   // don't allow copying, we should always create a new value.
   store_value(store_value&) = delete;
   store_value(const store_value&) = delete;
 
-  ~store_value() { delete _data; }
+  ~store_value() { delete[] _data; }
 
   const char* data() { return _data; }
   size_t size() { return _size; }
@@ -38,12 +43,9 @@ inline value_ptr make_value_ptr(const char* data, size_t size) {
   return seastar::make_lw_shared<store_value>(data, size);
 }
 
-// copies contents of the provided 'data' pointer to a new memory location, to
-// be owned by the newly created store_value.
 inline value_ptr make_value_ptr_by_copy(const char* data, size_t size) {
-  char* buf = new char[size];
-  memcpy(buf, data, size);
-  return make_value_ptr(buf, size);
+  return make_value_ptr(data, size);
+}
 
 class store_insert_entry {
   const std::string _key;
